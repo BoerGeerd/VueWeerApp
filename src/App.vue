02707e96ddb6c396b8a7cbb6621cf6e25a5d2720
geerd van Rooijen <rv662380@edu.rocrivor.nl>
2022-03-25 09:47:37 +0100

    updatw

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+    <main>
     <div class="search-box">
       <input type="text" class="search-bar" placeholder="Zoeken...">
 
     </div>
+    </main>
   </div>
 </template>
 <script>
@@ -42,7 +44,32 @@ body{
 main{
   min-height: 100vh;
   padding: 25px;
-  background-image: linear-gradient(to bottom,);
   
 }
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus{
+  box-shadow:0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
 </style>
