2b8830be57fa3a0dd23627335685d8a3ef67b3bd
geerd van Rooijen <rv662380@edu.rocrivor.nl>
2022-04-08 08:57:53 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
     <div class="search-box">
-      <input type="text" class="search-bar" placeholder="Zoeken...">
+      <input 
+       type="text"
+       class="search-bar"
+       placeholder="Zoeken..."
+       v-model="query"
+       @keypress="fetchWeather"
+      >
 
     </div>
-    <div class="weather-wrap">
+    <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
     <div class="location-box">
-      <div class="location">Tiel, Nederland</div>
+      <div class="location">{{weather.name}}, {{ weather.sys.country }}</div>
       <div class="date">vrijdag 25 maart 2022</div>
     </div>
   </div>
   <div class="weather-box">
     <div class="temp">15C</div>
-    <div class="weather">Spring</div>
+    <div class="weather">Zonnig</div>
   </div>
     </main>
   </div>
@@ -24,8 +30,24 @@ export default {
   name: 'app',
   data () {
     return {
-      api_key: '5901b71ee509629f421b590fbf5eb5a2'
+      api_key: '5901b71ee509629f421b590fbf5eb5a2',
+      url_base:'https://openweathermap.org/data/2.5/',
+      query: '',
+      weather:{}
       
+    }
+  },
+  methods:{
+    fetchWeather (e){
+      if (e.key == "Enter") {
+         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+         .then(res => {
+           return res.json();
+         }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
     }
   }
 
@@ -47,7 +69,7 @@ body{
 
 }
 #app{
-  background-image:url('./assets/coldbackground.jpg');
+  background-image:url('./assets/summerbackground.jpg');
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
