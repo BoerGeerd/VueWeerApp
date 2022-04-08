c00cb6616d944a6f993a5e6a972a3dd0f8f16aac
geerd van Rooijen <rv662380@edu.rocrivor.nl>
2022-04-08 09:03:22 +0200

    allahu akbar

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,13 @@
     <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
     <div class="location-box">
       <div class="location">{{weather.name}}, {{ weather.sys.country }}</div>
-      <div class="date">vrijdag 25 maart 2022</div>
+      <div class="date">{{dateBuilder() }}</div>
     </div>
-  </div>
+  
   <div class="weather-box">
-    <div class="temp">15C</div>
-    <div class="weather">Zonnig</div>
+    <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+    <div class="weather">{{ weather.weather[0].main }}</div>
+  </div>
   </div>
     </main>
   </div>
